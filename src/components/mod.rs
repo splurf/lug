f7869162b5e01dcfb8bb16a892270ce629079745
splurf <evandotschwartzentruber@gmail.com>
2025-04-11 14:14:54 -0400

    custom `<a>` impl

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,1 +1,14 @@
+use leptos::prelude::*;
 
+#[component]
+pub fn A(
+    href: &'static str,
+    #[prop(optional)] style: &'static str,
+    children: Children,
+) -> impl IntoView {
+    view! {
+        <a href=href style=style target="_blank" rel="noopener noreferrer">
+            {children()}
+        </a>
+    }
+}
